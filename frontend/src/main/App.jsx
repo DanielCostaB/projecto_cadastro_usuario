053b1b589bcdc708d546187c32fc9a067b00903f
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-20 17:52:32 -0300

    Add firts files routes configuration

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HasRouter } from 'react-router-dom'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/templates/Footer'
 
 export default props =>
-<div className='app'>
-  <Logo />
-  <Nav />
-  <Home />
-  <Footer />
-</div>
+  <HasRouter>
+    <div className='app'>
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </HasRouter>
