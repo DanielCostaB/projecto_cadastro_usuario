c5ad84544f1337f2477c363b64d6cd6f99ca07e9
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-21 09:40:37 -0300

    Add config routes App Nav and Logo

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
-import { HasRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
@@ -10,11 +10,11 @@ import Routes from './Routes'
 import Footer from '../components/templates/Footer'
 
 export default props =>
-  <HasRouter>
+  <BrowserRouter>
     <div className='app'>
       <Logo />
       <Nav />
       <Routes />
       <Footer />
     </div>
-  </HasRouter>
+  </BrowserRouter>
